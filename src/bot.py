0e612ed6c2ac90b332f7446f8bcb7bc3d5af6cb5
Quadatz <quadath@gmail.com>
2022-05-26 21:39:02 +0300

    hunger

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,9 @@
-import discord
-import json
+import math
 from random import randrange
+import discord,asyncio,os
+from discord.ext import tasks
+import time
+import json
 from tinydb import TinyDB, Query
 from discord.ext import commands
 
@@ -20,30 +23,77 @@ query = Query()
     # if ctx.author != bot.user:
         # if ctx.channel.name == "моя-свинюшня":
             # await ctx.reply(ctx.content)
+
 @bot.command()
 async def создать_хохла(ctx: commands.Context, name):
-    if not getPigWithId(ctx.author.id)
-        weight = randrange(5) + 2
-        db.insert({'name': name, 'weight': weight, 'owner': str(ctx.author.id)})
-        await ctx.reply("Теперь вы обладатель поросёнка. Его зовут " + name + ". Он весит всего " + str(weight) + " кг.")
+    if not db.search(query.owner == str(ctx.author.id)):
+        weight = randrange(5) + 5
+        db.insert({'name': name, 'satiety': 0.1, 'max-satiety': math.sqrt(weight) / 2, 'weight': weight, 'food': '200', 'owner': str(ctx.author.id)})
+        await ctx.reply("Теперь вы обладатель поросёнка. Его зовут " + name + ". Он весит всего " + str(weight) + " кг. Он голоден, покормите его")
     else: 
         await ctx.reply("У вас уже есть хряк")
 @bot.command()
 async def покормить(ctx: commands.Context, food):
+    await feed(int(food), str(ctx.author.id), ctx)
+@bot.command()
+async def хряк(ctx: commands.Context):
     pig = getPigWithId(ctx.author.id)
-    feed(pig["weight"], str(ctx.author.id))
-    pig = getPigWithId(ctx.author.id)
-    await ctx.reply("Теперь ваш хряк весит " + str(pig["weight"]) + " кг")
+    satiety = pig['satiety']
+    max_satiety = pig['max-satiety']
+    status = ''
+    if satiety >= max_satiety:
+        status = 'Хряк объелся и спит.'
+    elif satiety < max_satiety and satiety > (max_satiety * 0.75):
+        status = 'Хряк непротив покушать'
+    else:
+        status = 'Хряк голоден!'
+    await ctx.reply(f"Имя: {pig['name']} \nВес: {pig['weight']} кг\n {status}")
+
+@tasks.loop(seconds=1)
+async def change_status():
+    if db.search(query.owner != ""):
+        for val in db.all():
+            db.update({'satiety': val['satiety'] * 0.999}, query.owner == val['owner'])
+change_status.start()
+
+    # channel = bot.get_channel = xxx
+    # await bot.change_presence(activity=discord.Game('online'))
+    # print('test')
+    # await bot.channel.send('here')
 
 @bot.command()
 async def context(ctx: commands.Context):
     print(ctx.message)
     await ctx.reply("Author is " + str(ctx.author) + " id: " + str(ctx.author.id))
 
-def feed(weight, owner): 
+async def feed(food, owner, ctx): 
     pig = getPigWithId(owner)
-    db.update({"weight": pig["weight"] + randrange(10)}, query.owner == owner)
+    satiety = pig['satiety']
+    max_satiety = pig['max-satiety']
+    weight = pig['weight']
+    
+    if food > (max_satiety * 2):
+        await ctx.reply('Хряк не может съесть так много')
+        return
+
+    if satiety <= max_satiety * 0.75:
+        db.update({'satiety': satiety + food}, query.owner == owner)
+        db.update({'max-satiety': math.sqrt(weight) / 2}, query.owner == owner)
+        db.update({'weight': pig['weight'] + math.sqrt(food) * (randrange(100) / 50)}, query.owner == owner)
+        pig = getPigWithId(owner)
+        if pig['satiety'] >= pig['max-satiety']:
+            await ctx.reply(f'Ваш хряк наелся. Теперь он весит {math.ceil(pig["weight"])} кг!')
+        else: 
+            await ctx.reply('Хряк просит больше')        
+    else:
+        await ctx.reply('Хряк отказался')
 def getPigWithId(id):
     return db.search(query.owner == str(id))[0]
+
+# while True:
+#     if db.search(query.owner > 0):
+#         for val in db.all():
+#             db.update({'satiety': val['satiety'] * 0.999}, query.owner == val['owner'])
+#     time.sleep(1)
 
 bot.run(config['token'])
